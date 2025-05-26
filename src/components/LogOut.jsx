12f087d5e45f8d23dddc67b7dbca8fae3ae9380b
AK-PHOENIX <tigeraman55555@gmail.com>
2025-05-26 20:00:13 +0530

    Last:

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -54,11 +54,7 @@ const LogOut = () => {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
+            
           </a>
         </div>
         <div className="flex lg:hidden">
--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -52,11 +52,6 @@ const Pricing = () => {
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
             <span className="sr-only">Your Company</span>
-            <img
-              alt=""
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-8 w-auto"
-            />
           </a>
         </div>
         <div className="flex lg:hidden">
